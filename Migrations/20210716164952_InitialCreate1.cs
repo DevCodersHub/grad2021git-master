91e4b86f2c9e26c27145654bf2581c69ef6c3c28
eman rashwan <eashedeman54@outlook.com>
2021-07-17 16:27:36 +0200

    new

---
+++ Migrations/20210716164952_InitialCreate1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace grad2021.Migrations
+{
+    public partial class InitialCreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SelectionStatus",
+                table: "Branches",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Branches",
+                keyColumn: "BranchName",
+                keyValue: "الهندسة الصناعية",
+                column: "SelectionStatus",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Branches",
+                keyColumn: "BranchName",
+                keyValue: "الهندسة المدنية",
+                column: "SelectionStatus",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Branches",
+                keyColumn: "BranchName",
+                keyValue: "الهندسة المعمارية",
+                column: "SelectionStatus",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Branches",
+                keyColumn: "BranchName",
+                keyValue: "الهندسة الميكانيكية",
+                column: "SelectionStatus",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Branches",
+                keyColumn: "BranchName",
+                keyValue: "هندسة الإلكترونيات والاتصالات الكهربية",
+                column: "SelectionStatus",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Branches",
+                keyColumn: "BranchName",
+                keyValue: "هندسة الإنتاج والتصميم الميكانيكي",
+                column: "SelectionStatus",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Branches",
+                keyColumn: "BranchName",
+                keyValue: "هندسة الحاسبات والنظم",
+                column: "SelectionStatus",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Branches",
+                keyColumn: "BranchName",
+                keyValue: "هندسة القوى الميكانيكية",
+                column: "SelectionStatus",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Branches",
+                keyColumn: "BranchName",
+                keyValue: "هندسة القوى والآلات الكهربية",
+                column: "SelectionStatus",
+                value: 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SelectionStatus",
+                table: "Branches");
+        }
+    }
+}
